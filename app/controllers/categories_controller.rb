a7ccec1d457128bc2c93e693a63c5aac0140437f
dengzhichao <dengzhichao@qq.com>
2016-11-25 15:06:53 +0800

    完成了对图书分类的管理（包括对图书分类的增删改查功能）

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController <ApplicationController
+  
+  before_action :find_category_by_id, :only=>[:edit, :destroy, :update]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    Category.create :name=>params[:category][:name]
+    redirect_to categories_path
+  end
+
+  def edit
+  end
+
+  def update
+    @category.name = params[:category][:name]
+    @category.save
+    redirect_to categories_path
+  end
+
+  def destroy
+    @category.delete
+    redirect_to categories_path
+  end
+
+  def find_category_by_id
+    @category = Category.find(params[:id])
+  end
+
+end
