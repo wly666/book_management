8c6ddf316cdd8807dff0af6831962c1ceba07518
dengzhichao <dengzhichao@qq.com>
2016-11-25 16:23:37 +0800

    完成了对图书标签的管理(包括对图书标签的增删改查)

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,36 @@
+class TagsController <ApplicationController
+  
+  before_action :find_tag_by_id, :only=>[:edit, :update, :destroy]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    Tag.create :name=>params[:tag][:name]
+    redirect_to tags_path
+  end
+
+  def edit
+  end
+
+  def update
+    @tag.name = params[:tag][:name]
+    @tag.save
+    redirect_to tags_path
+  end
+
+  def destroy
+    @tag.delete
+    redirect_to tags_path
+  end
+
+  def find_tag_by_id
+    @tag = Tag.find(params[:id])
+  end
+
+end
