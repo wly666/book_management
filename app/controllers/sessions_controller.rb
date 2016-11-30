5a91867fef5516e45c11c0083350f183b06f4330
dengzhichao <dengzhichao@qq.com>
2016-11-30 09:15:13 +0800

    增加了登陆和退出功能

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_phone_number(params[:phone_number])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to books_path, notice: "Logged in!"
+    else
+      flash.now.alert = "Email or password is invalid"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path, notice: "Logged out!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :categories
   resources :circulates
   resources :tags
+  resources :sessions
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
 end
