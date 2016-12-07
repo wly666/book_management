da53d3bd07cd2b2a2a7fe433bf90fb358a3196e2
dengzhichao <dengzhichao@qq.com>
2016-12-07 14:47:53 +0800

    修改了初始的管理员的信息

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 User.create!([
-  {name: "管理员", phone_number: 15111111111, email: "123456@qq.com", borrow_card: "qwertyuiop", current_sign_in_at: nil, last_sign_in_at: nil, sign_in_count: nil, password_digest: "$2a$10$kYu2RzTkldaXMLiSGgLCpexraxR7vYcIctvt27MbRW.3satrUhq42", role: "admin"}
+  {name: "管理员", phone_number: 15111111111, email: "123456@qq.com", borrow_card: nil, current_sign_in_at: nil, last_sign_in_at: nil, sign_in_count: nil, password_digest: "$2a$10$kYu2RzTkldaXMLiSGgLCpexraxR7vYcIctvt27MbRW.3satrUhq42", role: "admin"}
 ])
